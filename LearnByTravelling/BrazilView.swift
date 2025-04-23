9c3af7400490e22979172c3279ca7dcac6f0239a
Emre345 <emredemirkaya8951@gmail.com>
2025-04-23 19:13:46 +0300

    Map and some countries added

---
+++ LearnByTravelling/BrazilView.swift
@@ -0,0 +1,18 @@
+//
+//  BrazilView.swift
+//  LearnByTravelling
+//
+//  Created by Emre Demirkaya on 23.04.2025.
+//
+
+import SwiftUI
+
+struct BrazilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrazilView()
+}
