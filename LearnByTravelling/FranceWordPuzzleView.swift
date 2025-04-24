67b458b179147a6b825ab0cfb53b8112463c5a65
Emre345 <emredemirkaya8951@gmail.com>
2025-04-24 18:39:23 +0300

    Mini games added to some countries.

---
+++ LearnByTravelling/FranceWordPuzzleView.swift
@@ -0,0 +1,18 @@
+//
+//  FranceWordPuzzleView.swift
+//  LearnByTravelling
+//
+//  Created by Emre Demirkaya on 24.04.2025.
+//
+
+import SwiftUI
+
+struct FranceWordPuzzleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FranceWordPuzzleView()
+}
