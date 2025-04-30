2adf04b573665d7bb794aed261a0c6dc89402934
Emre345 <emredemirkaya8951@gmail.com>
2025-04-30 19:18:30 +0300

    Mini word puzzle game added to all countries

---
+++ LearnByTravelling/EgyptWordPuzzleView.swift
@@ -0,0 +1,18 @@
+//
+//  EgyptWordPuzzleView.swift
+//  LearnByTravelling
+//
+//  Created by Emre Demirkaya on 30.04.2025.
+//
+
+import SwiftUI
+
+struct EgyptWordPuzzleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EgyptWordPuzzleView()
+}
